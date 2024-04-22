4228f0cdecc082bb2e347f8f2f90ea95c5d43f26
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-04-22 22:35:36 +0100

    Adding templates and fix templates setting

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('home/', views.home_page)
+    path('', views.index)
 ]
--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # request -> response
-def home_page(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
