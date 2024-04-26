d17443cd950316ce2c5d3fe504b3386443a23d0a
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-04-26 16:29:55 +0100

    Create the payment and purchase models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,46 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plan_name = models.CharField(max_length=100)
+    plan_price = models.DecimalField(max_digits=10, decimal_places=2)
+    plan_duration = models.CharField(max_length=20)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    payment_date = models.DateTimeField(default=timezone.now)
+    payment_reference = models.CharField(max_length=32, null=False, editable=False)
+
+    def _generate_payment_reference(self):
+        """
+        Generate a random, unique payment reference using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the payment reference
+        if it hasn't been set already.
+        """
+        if not self.payment_reference:
+            self.payment_reference = self._generate_payment_reference()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Payment #{self.payment_reference} - {self.user.username} - {self.plan_name}"
+
+class PurchaseHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plan_name = models.CharField(max_length=100)
+    plan_price = models.DecimalField(max_digits=10, decimal_places=2)
+    plan_duration = models.CharField(max_length=20)
+    purchase_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.plan_name} - {self.purchase_date}"
