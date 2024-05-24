5cb5b1c7306871cd17c9e1cb288da43a9cbefed1
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2024-05-24 23:01:29 +0000

    Add Cloudfire storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,17 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = 'static'
 
+    def get_object_parameters(self, name):
+        return {
+            'CacheControl': 'max-age=86400',  # 1 day
+        }
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = 'media'
+
+    def get_object_parameters(self, name):
+        return {
+            'CacheControl': 'max-age=31536000',  # 1 year
+        }
--- django_eCommerce/settings.py
+++ django_eCommerce/settings.py
@@ -175,29 +175,26 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+if 'USE_R2' in os.environ:
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'zing-gym'
-    AWS_S3_REGION_NAME = 'us-east-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    R2_STORAGE_BUCKET_NAME = 'zing-gym-django'
+    R2_ACCESS_KEY_ID = os.environ.get('R2_ACCESS_KEY_ID')
+    R2_SECRET_ACCESS_KEY = os.environ.get('R2_SECRET_ACCESS_KEY')
+    R2_ENDPOINT_URL = 'https://2a9289dc0e3ad2f3ab67c3d94e9355fd.r2.cloudflarestorage.com/zing-gym-django'
 
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
+    # Cache control headers
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',  # 1 day
+    }
+
+    DEFAULT_FILE_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.MediaStorage'
+    R2_CUSTOM_DOMAIN = f'{R2_STORAGE_BUCKET_NAME}.r2.cloudflarestorage.com'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{R2_CUSTOM_DOMAIN}/static/'
+    MEDIA_URL = f'https://{R2_CUSTOM_DOMAIN}/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
