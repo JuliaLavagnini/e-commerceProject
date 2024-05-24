4f23b83689b7a948108b3588d0d715cfd5b6404c
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-05-24 05:56:49 +0100

    Completed deployment

--- django_eCommerce/settings.py
+++ django_eCommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^qu!sveqfa-dm-zj*0)iqyy#q-5ssmg9ee*%yhynyg19zg0^)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zing-gym-516505ed627e.herokuapp.com', 'localhost']
 
 
 # Application definition
