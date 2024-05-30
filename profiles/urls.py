d7a0dc5efb5e664d15ac18057b1615a1357c570a
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-05-30 12:03:39 +0100

    Fix cancel function

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('cancel_membership/<payment_reference>', views.cancel_membership, name='cancel_membership'),
+    path('cancel_membership/<str:payment_reference>', views.cancel_membership, name='cancel_membership'),
     
 ]
