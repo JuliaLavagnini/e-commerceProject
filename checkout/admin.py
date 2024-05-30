6c33bb57f040233eae6809e23e8c9f0a26a9e67f
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-05-30 15:41:28 +0100

    fix error

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,6 +4,6 @@ from .models import Payment
 
 class PaymentAdmin(admin.ModelAdmin):
     readonly_fields = ('payment_reference',)
-    list_display = ('user', 'payment_date','plan_name', 'plan_price', 'plan_duration','country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'email')
+    list_display = ('user', 'payment_date','plan_name', 'plan_price', 'plan_duration','country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'email', 'status')
 
 admin.site.register(Payment, PaymentAdmin)
