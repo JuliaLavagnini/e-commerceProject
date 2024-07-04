0c246306962ae6a4e1f5fbb7f4d3663babdd1596
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-07-04 13:29:02 +0100

    configure send emails

--- django_eCommerce/settings.py
+++ django_eCommerce/settings.py
@@ -104,10 +104,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-ACCOUNT_EMAIL_VERIFICATION = 'none'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
